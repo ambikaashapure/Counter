c2413d1a08f908e8b1e3a77f09c140e3b8ad4c57
Ambika <ambika.ashapure01@gmail.com>
2023-07-11 11:40:09 +0530

    better formatted code with comments

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React ,{useState} from 'react' ;
+import React, { useState } from 'react';
 
 
-function App() { 
+function App() {
+
+//Use State hook
   const [inputValue, setInputValue] = useState(0);
+
+//function to increment the value
 
   const incrementCount = (event) => {
     event.preventDefault();
-    setInputValue(inputValue+1);
+    setInputValue(inputValue + 1);
   };
 
+//function to decrement the value
+
   const decrementCount = (event) => {
-    if (inputValue<1){
+    if (inputValue < 1) {
       alert('invalid input')
     }
-    else{
-    event.preventDefault();
-    setInputValue(inputValue-1);
+    else {
+      event.preventDefault();
+      setInputValue(inputValue - 1);
     }
-   
+
   };
+
+//function to increment the value by 50
 
   const increamentCountByFifty = (event) => {
     event.preventDefault();
-    setInputValue(inputValue+50);
+    setInputValue(inputValue + 50);
   };
 
-  const handleChange=(event)=>{
+  const handleChange = (event) => {
     setInputValue(parseInt(event.target.value));
   };
 
-  
-  
+
+
   return (
     <>
-    <title>My Counter</title>
-    <h1 align = "center">
-    <div className="blank">COUNTER</div>
-    </h1>
-    <div className="container">
-      <form >
-      <label>Enter you number:
-      <input type="number" name="num" id="num" onChange={handleChange} />
-      </label>
-      <br/><button onClick ={incrementCount} type="submit"> Increment </button>
-      <button onClick ={decrementCount} type="submit"> Decrement </button>
-      <button onClick ={increamentCountByFifty} type="submit"> Increment by 50 </button><br/>
-      <label>Output:
-      <textarea type="number" name="num" id="num" value ={inputValue} />
-      </label>
-    </form>
-    </div>
+      <title>My Counter</title>
+      <h1 align="center">
+        <div className="blank">COUNTER</div>
+      </h1>
+      <div className="container">
+        <form >
+          <label>Enter you number:
+            <input type="number" onChange={handleChange} />
+          </label>
+          <br /><button onClick={incrementCount} type="submit"> Increment </button>
+          <button onClick={decrementCount} type="submit"> Decrement </button>
+          <button onClick={increamentCountByFifty} type="submit"> Increment by 50 </button><br />
+          <label>Output:
+            <textarea type="number" value={inputValue} />
+          </label>
+        </form>
+      </div>
     </>
   );
-  }
+}
 
 export default App;
