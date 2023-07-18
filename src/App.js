3cf29438b182474a3ac06abf681589b1b8c13d75
Ambika Ashapure <119685403+ambikaashapure@users.noreply.github.com>
2023-07-18 13:03:29 +0530

    axios-src

--- src/App.js
+++ src/App.js
@@ -1,67 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import './App.css';
-import React, { useState } from 'react';
+const App = () => {
+  const [data,newData] = useState([]);
 
-
-function App() {
-
-//Use State hook
-  const [inputValue, setInputValue] = useState(0);
-
-//function to increment the value
-
-  const incrementCount = (event) => {
-    event.preventDefault();
-    setInputValue(inputValue + 1);
-  };
-
-//function to decrement the value
-
-  const decrementCount = (event) => {
-    if (inputValue < 1) {
-      alert('invalid input')
-    }
-    else {
-      event.preventDefault();
-      setInputValue(inputValue - 1);
-    }
-
-  };
-
-//function to increment the value by 50
-
-  const increamentCountByFifty = (event) => {
-    event.preventDefault();
-    setInputValue(inputValue + 50);
-  };
-
-  const handleChange = (event) => {
-    setInputValue(parseInt(event.target.value));
-  };
-
-
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(rep => {
+        newData(rep.data);
+      })
+  },[]);
 
   return (
-    <>
-      <title>My Counter</title>
-      <h1 align="center">
-        <div className="blank">COUNTER</div>
-      </h1>
-      <div className="container">
-        <form >
-          <label>Enter you number:
-            <input type="number" onChange={handleChange} />
-          </label>
-          <br /><button onClick={incrementCount} type="submit"> Increment </button>
-          <button onClick={decrementCount} type="submit"> Decrement </button>
-          <button onClick={increamentCountByFifty} type="submit"> Increment by 50 </button><br />
-          <label>Output:
-            <textarea type="number" value={inputValue} />
-          </label>
-        </form>
-      </div>
-    </>
+    <div>
+      <h1>The data is :</h1>
+
+      {data.map(info => (
+        <div>
+          <h2>{info.id}. {info.title}</h2>
+          <p>{info.body}</p>
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
